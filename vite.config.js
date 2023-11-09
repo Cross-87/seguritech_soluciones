6cab90020908b5a0cb73512d2b87f8938b3241fc
Cross-87 <racq_2016@outlook.com>
2023-11-08 18:21:07 -0600

    deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://github.com/Cross-87/seguritech_soluciones.git",
+  base:"/seguritech_soluciones/",
   plugins: [react()]
 })
